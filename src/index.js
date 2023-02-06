661cb439cee7f958a8808bdabb02a7239e9c832b
Gspace Developers <115160413+GspaceDevelopers@users.noreply.github.com>
2023-02-06 09:06:43 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ class App {
     this.server = express();
 
     mongoose.connect(
-      "mongodb+srv://vittoria:vittoria@cluster0.ao0fksp.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://vittoriavitt:vittoriavitt@cluster0.i4bpexd.mongodb.net/?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
