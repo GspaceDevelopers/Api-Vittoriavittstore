111de4628e62addc6a45568c7756109294006070
Gspace Developers <115160413+GspaceDevelopers@users.noreply.github.com>
2023-02-06 08:26:01 -0300

    Add files via upload

--- src/controllers/Categoriascontroller.js
+++ src/controllers/Categoriascontroller.js
@@ -15,9 +15,10 @@ class Categoriascontroller {
 
         return res.json(categorialist)
     }
-     async delete(req, res) {
-         const { _id } = req.params
-        const categorialist = await Categorias.deleteOne({_id:_id})
+
+    async delete(req, res) {
+        const { _id } = req.params
+        const categorialist = await Categorias.deleteOne({ _id: _id })
 
         return res.json(categorialist)
     }
