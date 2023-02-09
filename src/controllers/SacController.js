2bc547a6c53dd219257fbd10aa8cbaff3797e1de
Gspace Developers <115160413+GspaceDevelopers@users.noreply.github.com>
2023-02-09 18:27:21 -0300

    Update SacController.js

--- src/controllers/SacController.js
+++ src/controllers/SacController.js
@@ -2,15 +2,15 @@ import Sac from "../schemas/Sac";
 
 class Saccontroller {
     async post(req, res) {
-        const { nome, email, mensagem,telefone,cpf,assunto,numeropedido } = req.body;
+        const { nome, email, mensagem, telefone, cpf, assunto, numeropedido } = req.body;
         const saclist = await Sac.create({
             nome: nome,
             email: email,
             mensagem: mensagem,
-              telefone: telefone,
-        cpf: cpf,
-        assunto: assunto,
-        numeropedido:numeropedido
+            telefone: telefone,
+            cpf: cpf,
+            assunto: assunto,
+            numeropedido: numeropedido
         })
         return res.json(saclist)
     }
