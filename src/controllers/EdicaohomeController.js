1552147ddf0bf429e979e4a530bd2d21621e707a
Gspace Developers <115160413+GspaceDevelopers@users.noreply.github.com>
2023-02-13 07:34:41 -0300

    Update EdicaohomeController.js

--- src/controllers/EdicaohomeController.js
+++ src/controllers/EdicaohomeController.js
@@ -2,9 +2,11 @@ import Edicaohome from "../schemas/Edicaohome";
 
 class EdicaohomeController {
     async post(req, res) {
-        const { componentetexto1,bannermodalpromocao,bannercentralhome, componentelogos: { logo1, logo2, logo3, logo4, logo5, logo6, logo7, }, backgoundhome: { background1, background2, background3, backgroundmobile1, backgroundmobile2, backgroundmobile3 }, componentetexto2, banners: { banner1, banner2, banner3, banner4, banner5, banner6, banner7 } } = req.body;
+        const { componentetexto1,parcelas,bannerpaginaprod,bannermodalpromocao,bannercentralhome, componentelogos: { logo1, logo2, logo3, logo4, logo5, logo6, logo7, }, backgoundhome: { background1, background2, background3, backgroundmobile1, backgroundmobile2, backgroundmobile3 }, componentetexto2, banners: { banner1, banner2, banner3, banner4, banner5, banner6, banner7 } } = req.body;
         const Edicaolist = await Edicaohome.create({
             componentetexto1: componentetexto1,
+            parcelas:parcelas,
+            bannerpaginaprod:bannerpaginaprod,
              bannermodalpromocao:bannermodalpromocao ,
             bannercentralhome:bannercentralhome,
             componentelogos: {
@@ -39,9 +41,11 @@ class EdicaohomeController {
     }
     async update(req, res) {
         const { _id: _id } = req.params;
-        const { componentetexto1,bannermodalpromocao,bannercentralhome, componentelogos: { logo1, logo2, logo3, logo4, logo5, logo6, logo7, }, backgoundhome: { background1, background2, background3, backgroundmobile1, backgroundmobile2, backgroundmobile3 }, componentetexto2, banners: { banner1, banner2, banner3, banner4, banner5, banner6, banner7 } } = req.body;
+        const { componentetexto1,parcelas,bannerpaginaprod,bannermodalpromocao,bannercentralhome, componentelogos: { logo1, logo2, logo3, logo4, logo5, logo6, logo7, }, backgoundhome: { background1, background2, background3, backgroundmobile1, backgroundmobile2, backgroundmobile3 }, componentetexto2, banners: { banner1, banner2, banner3, banner4, banner5, banner6, banner7 } } = req.body;
         const Edicaolist = await Edicaohome.updateOne({ _id: _id }, {
             componentetexto1: componentetexto1,
+              parcelas:parcelas,
+            bannerpaginaprod:bannerpaginaprod,
             bannermodalpromocao:bannermodalpromocao,
             bannercentralhome:bannercentralhome,
             componentelogos: {
