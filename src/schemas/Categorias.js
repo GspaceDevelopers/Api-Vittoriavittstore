07c11771dc576edc2bdc86a96eb7d7e369655a82
Gspace Developers <115160413+GspaceDevelopers@users.noreply.github.com>
2023-02-15 16:04:56 -0300

    Update Categorias.js

--- src/schemas/Categorias.js
+++ src/schemas/Categorias.js
@@ -1,6 +1,10 @@
 import { model, Schema } from 'mongoose'
 
 const Categorias = new Schema({
-    categoria: String
+    categoria: String,
+    sub1:String,
+    sub2:String,
+    sub3:String,
+    sub4:String,
 })
 export default new model('Categorias', Categorias)
