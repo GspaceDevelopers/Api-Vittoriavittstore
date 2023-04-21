e90615c835bb3ebcc4638798b1a067c813aece25
Gspace Developers <115160413+GspaceDevelopers@users.noreply.github.com>
2023-04-21 10:59:50 -0300

    Update Sobre Controller

--- src/controllers/EditarSobre.js
+++ src/controllers/EditarSobre.js
@@ -2,9 +2,17 @@ import EditarSobreSchemas from "../schemas/EditarSobre";
 
 class EditarSobre {
   async post(req, res) {
-    const { sobre } = req.body;
-    const Edicaolist = await EditarSobreSchemas.create({ sobre: sobre });
+    const { sobre , banners : {banner1, banner2, banner3, banner4}} = req.body;
+    const Edicaolist = await EditarSobreSchemas.create({ sobre: sobre , 
+      banners: {
+        banner1: banner1,
+        banner2: banner2,
+        banner3: banner3,
+        banner4: banner4
+      }});
+    console.log(req.body);
     return res.json(Edicaolist);
+    
   }
   async update(req, res) {
     const { _id: _id } = req.params;
