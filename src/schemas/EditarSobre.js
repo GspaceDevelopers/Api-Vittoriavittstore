9bf94175e88b18849626782cc78e0dd074c6c5a0
Gspace Developers <115160413+GspaceDevelopers@users.noreply.github.com>
2023-04-21 10:58:45 -0300

    Update Editar Sobre Schema

--- src/schemas/EditarSobre.js
+++ src/schemas/EditarSobre.js
@@ -2,6 +2,12 @@ import { Schema, model } from "mongoose";
 
 const EditarSobre = new Schema({
   sobre: String,
+  banners: {
+    banner1: String,
+    banner2: String,
+    banner3: String,
+    banner4: String
+  }
 });
 
 export default model("EditarSobre", EditarSobre);
